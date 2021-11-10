0bdf3f36f5e58ae6be27c926eb36531f84aa43e3
Ryan Turner <turnerr8@wwu.edu>
2021-11-09 21:56:27 -0800

    need some style and more info

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $('.right').click(function(){
         $(this).css("background-color", "green");
         $('#info-1').show();
+        $('#material-quiz').show();
         $('.wrong').css("background-color", "");
         $('.wrong').css("color", "");
 
@@ -17,4 +18,23 @@ $(document).ready(function () {
     $('.quiz-option').click(function(){
         $(this).css("color", "white");
     })
+
+    //QUIZ FOR MATERIAL
+    $('.right-1').click(function(){
+        $(this).css("background-color", "green");
+        $('#material').show();
+        $('.wrong-1').css("background-color", "");
+        $('.wrong-1').css("color", "");
+
+    });
+
+    //wrong answer is chosen
+    $('.wrong-1').click(function(){
+        $(this).css("background-color", "red");
+    })
+
+    $('.quiz-option-1').click(function(){
+        $(this).css("color", "white");
+    })
+    
 });
